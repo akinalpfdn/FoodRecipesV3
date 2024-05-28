8da62eee20dbda234b7702e6402d2b8193c5f695
Akinalp Fidan <81157056+akinalpfdn@users.noreply.github.com>
2024-05-28 15:01:37 +0300

    todo eklendi

---
+++ app/src/main/java/com/example/foodrecipesv3/Todo.kt
@@ -0,0 +1,15 @@
+package com.example.foodrecipesv3
+
+class Todo {
+    /*
+    lvller için iconlar eklenicek
+    giriş ekranındaki butonlar düzenlenecek
+    fontlar düzenlenecek
+    tooltip düzenlenecek
+    action bara did you love it ekranı eklenecek
+    favori ekranı eklenecek
+    admin paneli eklenecek (tüm recipeler görülebilir olmalı)
+    raporlananlar paneli eklenecek
+    raporlama butonu eklenecek
+     */
+}
