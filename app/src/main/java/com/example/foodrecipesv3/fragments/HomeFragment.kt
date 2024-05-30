2a2995bc905a072a8c5a8ac9fd66efaabfed2c0b
Akinalp Fidan <81157056+akinalpfdn@users.noreply.github.com>
2024-05-30 23:59:40 +0300

    search bar arranged

--- app/src/main/java/com/example/foodrecipesv3/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/foodrecipesv3/fragments/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.example.foodrecipesv3.fragments
 
 import CustomTypefaceSpan
+import android.content.Context
+import android.content.res.Resources
 import android.graphics.Typeface
 import android.os.Bundle
 import android.text.SpannableString
@@ -11,6 +13,7 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.SearchView
 import android.widget.Spinner
@@ -48,6 +51,7 @@ class HomeFragment : Fragment() {
 
     //for ordering
     private lateinit var orderSpinner: Spinner
+    private lateinit var spinnerSearchOptions: Spinner
     private lateinit var ascendingIcon: ImageView
     private lateinit var descendingIcon: ImageView
     private var isAscending = false
@@ -85,10 +89,10 @@ class HomeFragment : Fragment() {
 
         //for ordering
         orderSpinner = view.findViewById(R.id.orderSpinner)
+        spinnerSearchOptions = view.findViewById(R.id.spinnerSearchOptions)
+        ascendingIcon = view.findViewById(R.id.ascendingIcon)
         ascendingIcon = view.findViewById(R.id.ascendingIcon)
         descendingIcon = view.findViewById(R.id.descendingIcon)
-
-
         progressBar = activity?.findViewById(R.id.progressBar)
         fetchRecipes(true)
 
@@ -116,7 +120,7 @@ class HomeFragment : Fragment() {
 
 
         val orderOptions = resources.getStringArray(R.array.order_options)
-
+        //sorting spinneri icin
         val adapter = object : ArrayAdapter<String>(requireContext(), R.layout.spinner_item, orderOptions) {
             override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
                 val view2 = super.getView(position, convertView, parent) as TextView
@@ -167,12 +171,96 @@ class HomeFragment : Fragment() {
         }
 
         //spinner.adapter = adapter
-
         adapter.setDropDownViewResource(R.layout.spinner_dropdown_item) // Custom layout for dropdown item
         orderSpinner.adapter = adapter
         setupOrderMenu()
 
+        //search spinneri icin
+        val search_options = resources.getStringArray(R.array.search_options)
+        val adapterSearch = object : ArrayAdapter<String>(requireContext(), R.layout.search_spinner_item, search_options) {
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+                val view2 = super.getView(position, convertView, parent) as TextView
+                setIcon(view2, position)
+                return view2
+            }
+            private fun setFontForOptionItem(textView: TextView, typeface: Typeface) {
+                val spannableTitle = SpannableString(textView.text)
+                spannableTitle.setSpan(CustomTypefaceSpan("", typeface), 0, spannableTitle.length, 0)
+                textView.text = spannableTitle
+            }
+            override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+                val view2 = super.getDropDownView(position, convertView, parent) as TextView
+                view2.text = search_options[position] // Show full text in dropdown
+                val typeface: Typeface? = ResourcesCompat.getFont(requireContext(), R.font.merienda)
+                typeface?.let { nonNullTypeface ->
+                    setFontForOptionItem(view2, nonNullTypeface)
+                }
+                return view2
+            }
+
+            private fun setIcon(view: TextView, position: Int) {
+                view.text = when (position) {
+                    0 -> "Title"
+                    1 -> "Hashtag"
+                    2 -> "Ingredients"
+                    else -> "Title"
+                }
+            }
+        }
+
+        adapterSearch.setDropDownViewResource(R.layout.spinner_dropdown_item) // Custom layout for dropdown item
+        spinnerSearchOptions.adapter = adapterSearch
+        val displayMetrics = Resources.getSystem().displayMetrics
+        val screenWidth = displayMetrics.widthPixels
+        val halfScreenWidth = screenWidth / 2
+        val searchView = view.findViewById<SearchView>(R.id.searchView)
+        searchView.setOnSearchClickListener {
+            // User has clicked the search icon to expand the SearchView
+            val paramsSpinner = spinnerSearchOptions.layoutParams as LinearLayout.LayoutParams
+            paramsSpinner.width = 0 // Use 0dp to respect weights in LinearLayout
+            spinnerSearchOptions.layoutParams = paramsSpinner
+            spinnerSearchOptions.visibility = View.GONE
+/*
+            // Adjust SearchView layout parameters
+            val params = searchView.layoutParams as LinearLayout.LayoutParams
+            params.weight = 1.0f // Ensure it uses the space responsibly
+            params.width = 0 // Use 0dp to respect weights in LinearLayout
+            searchView.layoutParams = params
+            eski kod
+            */
+            val params = spinnerSearchOptions.layoutParams
+            params.width = halfScreenWidth.toInt()
+               // 30.dpToPx(requireContext())  // Convert 30 dp to pixel
+            searchView.layoutParams = params
+
+
+            try {
+                val field = SearchView::class.java.getDeclaredField("mSearchSrcTextView")
+                field.isAccessible = true
+                val textView = field.get(searchView) as? TextView
+                textView?.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+
+        }
+
+        searchView.setOnCloseListener {
+            // User has clicked the close button to collapse the SearchView
+            val params = searchView.layoutParams as LinearLayout.LayoutParams
+            params.weight = 1.0f // Ensure it uses the space responsibly
+            params.width = 0 // Use 0dp to respect weights in LinearLayout
+            searchView.layoutParams = params
+            spinnerSearchOptions.visibility = View.VISIBLE
+            val paramsSpinner = spinnerSearchOptions.layoutParams as LinearLayout.LayoutParams
+            paramsSpinner.width = LinearLayout.LayoutParams.WRAP_CONTENT  // or another value based on your design
+            spinnerSearchOptions.layoutParams = paramsSpinner
+            false
+        }
+
     }
+    fun Int.dpToPx(context: Context): Int = (this * context.resources.displayMetrics.density).toInt()
     private fun setupOrderMenu() {
         orderSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
